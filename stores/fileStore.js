f6065a00ad4df35b942650ce1ee60b21edf2bda2
MATIJA SIMIN <marcussimon12082015@hotmail.com>
2019-07-21 11:39:58 +0000

    Automatically backed up by Learn

--- stores/fileStore.js
+++ stores/fileStore.js
@@ -3,12 +3,25 @@
 import Store from './Store';
 
 class FileStore extends Store{
-  constructor(initialState){
-    super();
-    this.state = {
-      files:initialState.push[''],
-      selectedFileIndex: 0
-    };
+  addFile(){
+    let files = [...this.getState(),''];
+    this.setState(files);
+  }
+  removeFile(index){
+    let files = this.getState();
+    if (files.length === 1) {
+      let startFiles = [''];
+      this.setState(startFiles);
+    }else{
+      files.splice(index,1);
+      this.setState(files);
+    }
+  }
+  updateFile(index,file){
+    let files = this.getState();
+    files[index] = file;
+    let updatedFiles = [...files];
+    this.setState(updatedFiles);
   }
 }
 
